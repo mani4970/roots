b046cd3be8357e132f41bb335cdd372b98c16300
Chungman Jeong <cookiko313@gmail.com>
2026-05-06 12:46:38 +0200

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,6 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
-const appUrl = process.env.CAPACITOR_SERVER_URL || "https://christian-roots.com";
+const appUrl = process.env.CAPACITOR_SERVER_URL || "https://www.christian-roots.com";
 
 const config: CapacitorConfig = {
   appId: "com.rootspuce.app",
@@ -10,6 +10,10 @@ const config: CapacitorConfig = {
     url: appUrl,
     cleartext: false,
     androidScheme: "https",
+    allowNavigation: [
+      "christian-roots.com",
+      "www.christian-roots.com",
+    ],
   },
   plugins: {
     SplashScreen: {
